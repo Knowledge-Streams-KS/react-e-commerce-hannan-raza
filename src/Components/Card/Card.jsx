f5ccb0ee345c8cb3a577313a903ea3b32e497d29
Hannan <hannanraza26@gmail.com>
2023-02-07 19:28:12 +0500

    styled a little bit

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Card.css'
 
 
 const Card = (props) => {
@@ -7,11 +8,12 @@ const Card = (props) => {
 
 return (
   <div className="product">
+    <div className="products">
     <h1>{props.title}</h1>
     <h1>{props.price}</h1>
     {/* <h1>{props.description}</h1> */}
-    <img src={props.image}  />
-  
+    <img src={props.image}/>
+    </div>
    </div>
   )
 };
